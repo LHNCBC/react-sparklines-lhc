540c40cbe90aac8a510c159ad9579bd9cb8b3307
Ye Wang <wangye@nih.gov>
2019-01-10 16:46:12 -0500

    update version and package.json

--- src/SparklinesReferenceLine.js
+++ src/SparklinesReferenceLine.js
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import * as dataProcessing from './dataProcessing';
-//import dataToPoints from './dataProcessing/dataToPoints';
-
 
 export default class SparklinesReferenceLine extends React.Component {
 
